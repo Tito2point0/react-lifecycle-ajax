d053f521faee0895af01f8febf496d840dc337d8
sanjiro <defconx00@gmail.com>
2023-08-24 19:14:39 -0400

    rendering data and also inserting fetchalltodo functions

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  
+  state = {
+    todos: [],
+    error: '',
+  }
+  fetchAlltodos = () => {
+    axios.get(URL)
+      .then(res => {
+      this.setState({...this.state,  todos: res.data.data })
+      })
+      .catch(err => {
+ this.setState({...this.state, error:err.response.data.message})
+    })
+  } 
+  componentDidMount() {
+    this.fetchAlltodos()  }
   render() {
-    return null
+    return (
+      <div>
+        <div id="error"> Error {this.state.error}</div>
+        <div id="todos">
+          <h2> Todos: </h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id="todoForm">
+          <input type='text' placeholder='Type todo here'></input>
+          <input type='submit'></input>
+          <button> Clear Component</button>
+
+        </form>
+      </div>
+    )
   }
 }
